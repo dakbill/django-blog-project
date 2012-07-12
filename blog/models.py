069a02e829f2b72063de962f249d4ac21e14fb07
dakbill <dakbill@yahoo.com>
2012-07-12 16:53:46 +0000

    Am feeling lucky

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib import admin
+import datetime
 class Post(models.Model):
     title = models.CharField(max_length=60)
     body = models.TextField()
-    created = models.DateField()
-    updated = models.DateField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    
     def body_first_60(self):
       return self.body[:60]
     def __unicode__(self):
@@ -12,8 +14,8 @@ class Post(models.Model):
 class Comment(models.Model):
     body = models.TextField()
     author = models.CharField(max_length=60)
-    created = models.DateField()
-    updated = models.DateField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
     post = models.ForeignKey(Post,related_name='post')
 class PostAdmin(admin.ModelAdmin):
     list_display=('title','created','updated')
--- blog/views.py
+++ blog/views.py
@@ -5,22 +5,37 @@ This code should be copied and pasted into your blog/views.py file before you be
 from django.template import Context, loader
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-
 from models import Post, Comment 
-
+from django.forms import ModelForm
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 
 def post_list(request):
 	posts = Post.objects.all()
 	t = loader.get_template('blog/post_list.html')
 	c = Context({'posts':posts })
 	return HttpResponse(t.render(c))
-
-
+class CommentForm(ModelForm):
+	class Meta:
+		model=Comment
+		exclude=['post']
+@csrf_exempt
 def post_detail(request, id, showComments=False):
-	comments=Comment.objects.filter(pk=id)
-	t = loader.get_template('blog/post_detail.html')
-	c = Context({'comments':comments })
-	return HttpResponse(t.render(c))
+		wanted_post=Post.objects.get(pk=id)
+		if request.method == 'POST':
+			comment=Comment(post=wanted_post)	
+			form = CommentForm(request.POST,instance=comment)
+			if form.is_valid():
+				form.save()
+			else:
+				print 'invalid'
+			return HttpResponseRedirect(request.path)
+		else:
+			form = CommentForm()
+		comments=Comment.objects.filter(post=wanted_post)
+		t = loader.get_template('blog/post_detail.html')
+		c = Context({'comments':comments,'form':form,'wanted_post':wanted_post })
+		return HttpResponse(t.render(c))
 def post_search(request, term):
     pass
 
